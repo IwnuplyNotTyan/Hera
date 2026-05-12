1caa88309dbb97dad69529b5e0b836fbe2da652c
IwnuplyNotTyan <wolflast21@gmail.com>
2026-05-12 22:04:02 +0300

    refactor: move mouse render state into Model fields

--- core/mouse.go
+++ core/mouse.go
@@ -1,21 +1,20 @@
 package generate
 
-var layoutElements []Element
-var gridOffsetX, gridOffsetY int
-
-func resetLayout() {
-	layoutElements = nil
+func (m *Model) resetLayout() {
+	m.layoutElements = nil
+	m.gridOffsetX = 0
+	m.gridOffsetY = 0
 }
 
-func trackElement(elem Element) {
-	layoutElements = append(layoutElements, elem)
+func (m *Model) trackElement(elem Element) {
+	m.layoutElements = append(m.layoutElements, elem)
 }
 
-func hitTest(screenX, screenY int) *Element {
-	absX := screenX - gridOffsetX
-	absY := screenY - gridOffsetY
-	for i := range layoutElements {
-		elem := &layoutElements[i]
+func (m *Model) hitTest(screenX, screenY int) *Element {
+	absX := screenX - m.gridOffsetX
+	absY := screenY - m.gridOffsetY
+	for i := range m.layoutElements {
+		elem := &m.layoutElements[i]
 		if absX >= elem.X && absX < elem.X+elem.Width &&
 			absY >= elem.Y && absY < elem.Y+elem.Height {
 			return elem
@@ -24,10 +23,5 @@ func hitTest(screenX, screenY int) *Element {
 	return nil
 }
 
-func cellWidth() int {
-	return 3
-}
-
-func cellHeight() int {
-	return 1
-}
+func cellWidth() int  { return 3 }
+func cellHeight() int { return 1 }
--- core/struct.go
+++ core/struct.go
@@ -81,37 +81,39 @@ const (
 )
 
 type Model struct {
-	Theme            *bubbletint.Registry
-	ThemeName        string
-	Styles           Styles
-	EnableBackground bool
-	CenterWindow     bool
-	TerminalWidth    int
-	TerminalHeight   int
-	Screen           Screen
-	MenuSelected     int
-	EasterEgg        string
-	ThemeSearch      bool
-	ThemeQuery       string
-	LastSearchQuery  string
-	AvailableThemes  []string
-	Players          []Player
-	Enemys           []Enemy
-	CurrentPlayer    int
-	CurrentEnemy     int
-	CursorX, CursorY int
-	Walls            map[Point]bool
-	Water            map[Point]bool
-	FireTiles        map[Point]int
-	SmokeTiles       map[Point]int
-	ShootMode        bool
-	UltMode          bool
-	UltAxis          string
-	Moved            bool
-	Shot             bool
-	keys             keyMap
-	help             help.Model
-	EnemyTurn        bool
-	EnemyIdx         int
-	Localizer        i18n.Localizer
+	Theme                    *bubbletint.Registry
+	ThemeName                string
+	Styles                   Styles
+	EnableBackground         bool
+	CenterWindow             bool
+	TerminalWidth            int
+	TerminalHeight           int
+	Screen                   Screen
+	MenuSelected             int
+	EasterEgg                string
+	ThemeSearch              bool
+	ThemeQuery               string
+	LastSearchQuery          string
+	AvailableThemes          []string
+	Players                  []Player
+	Enemys                   []Enemy
+	CurrentPlayer            int
+	CurrentEnemy             int
+	CursorX, CursorY         int
+	Walls                    map[Point]bool
+	Water                    map[Point]bool
+	FireTiles                map[Point]int
+	SmokeTiles               map[Point]int
+	ShootMode                bool
+	UltMode                  bool
+	UltAxis                  string
+	Moved                    bool
+	Shot                     bool
+	keys                     keyMap
+	help                     help.Model
+	EnemyTurn                bool
+	EnemyIdx                 int
+	Localizer                i18n.Localizer
+	layoutElements           []Element
+	gridOffsetX, gridOffsetY int
 }
